216d0bcc54620655f637d49bb0aa6e03713cdc6a
cclabdis <cclabdis@gmail.com>
2023-05-09 21:01:35 -0300

    first commit

---
+++ src/header/header.js
@@ -0,0 +1,28 @@
+<header>
+        <div class="container">
+            <nav class="navbar">
+                <div class="menu-toggle">
+                    <i class="fas fa-bars"></i>
+                    <i class="fas fa-times"></i>
+                </div>
+                <a href="index.html" class="logo">Tele Coleta</a>
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <a href="index.html" class="nav-link active">Sobre nós</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="about.html" class="nav-link">Nossa missão</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="about.html#reservation" class="nav-link">Serviços</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="bakers.html" class="nav-link">Galeria</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="bakers.html" class="nav-link">Carta de apresentação</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
